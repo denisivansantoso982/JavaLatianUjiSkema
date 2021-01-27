da4681366390f1125d4dcdd8441c3d46deb66456
Denis Ivan Santoso <61743345+denisivansantoso982@users.noreply.github.com>
2021-01-27 11:25:18 +0700

    Delete Statistical.java

--- Statistical.java
+++
@@ -1,9 +0,0 @@
-package latianujiskema;
-
-public interface Statistical {
-    
-   double hitungRerata(Siswa data1[]);
-   void sorting(Siswa data2[]);
-   void searching(Siswa data3[]);
-           
-}
