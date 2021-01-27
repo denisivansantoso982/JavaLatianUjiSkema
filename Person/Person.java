33b5745b925d6339560d84c1b2aca3ec58177a1e
Denis Ivan Santoso <denisivansantoso@gmail.com>
2021-01-27 11:31:36 +0700

    initial commit

---
+++ Person/Person.java
@@ -0,0 +1,13 @@
+package Person;
+
+public class Person {
+    protected String nama;
+    
+    public Person() {
+        this.nama = "";
+    }
+    
+    public void infoPerson() {
+        System.out.println(this.nama);
+    }
+}
---
+++ Statistical/Statistical.java
@@ -0,0 +1,11 @@
+package Statistical;
+
+import Person.Siswa;
+
+public interface Statistical {
+    
+   double hitungRerata(Siswa data1[]);
+   void sorting(Siswa data2[]);
+   void searching(Siswa data3[]);
+           
+}
